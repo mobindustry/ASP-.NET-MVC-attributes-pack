71af4fff936009aa157ec9edfef48dbccc9bd858
Julia <yulia.bobrik@mobindustry.net>
2014-10-23 13:53:34 +0300

    MinAgeAttribute is created

---
+++ Validation/MinAgeAttribute/MinAgeAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Attributes
+{
+	[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+	public class MinAgeAttribute : ValidationAttribute
+	{
+		private readonly int _minAge;
+
+		public MinAgeAttribute(int minAge)
+		{
+			_minAge = minAge;
+		}
+
+
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			if (value is DateTime)
+			{
+				DateTime birthDate = (DateTime) value;
+				DateTime maxDate = DateTime.Today.AddYears(-_minAge);
+
+				if (maxDate < birthDate)
+					return new ValidationResult(String.Format("You must be at least {0} years old", _minAge));
+			}
+
+			return ValidationResult.Success;
+		}
+	}
+}
