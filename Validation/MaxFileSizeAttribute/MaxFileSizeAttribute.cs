91a376f2bf054e109caf63209ce7493dd99d8472
Julia <yulia.bobrik@mobindustry.net>
2014-11-04 10:45:02 +0200

    Update MaxFileSizeAttribute.cs

--- Validation/MaxFileSizeAttribute/MaxFileSizeAttribute.cs
+++ Validation/MaxFileSizeAttribute/MaxFileSizeAttribute.cs
@@ -9,7 +9,13 @@ namespace Attributes.Validation
 	[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
 	public class MaxFileSizeAttribute : ValidationAttribute, IClientValidatable
 	{
-		public enum Units { Byte, Kilobyte, Megabyte, Gigabyte }
+		public enum Units
+		{
+			Byte = 1,
+			Kilobyte = 1024,
+			Megabyte = 1024*1024,
+			Gigabyte = 1024*1024*1024
+		}
 
 		private readonly Dictionary<Units, string> _unitNames = new Dictionary<Units, string>
 			{
@@ -21,11 +27,30 @@ namespace Attributes.Validation
 
 
 		private readonly long _maxFileSize;
+		private Units? _formatUnit;
 
 		/// <summary>
-		/// FormatUnit is used for displaying user friendly file size value
+		/// FormatUnit is used for displaying user friendly file size value. If not specified detected automatically
 		/// </summary>
-		public Units FormatUnit { get; set; }
+		public Units FormatUnit
+		{
+			get
+			{
+				if (!_formatUnit.HasValue)
+				{
+					double value = _maxFileSize;
+
+					if (value >= (int)Units.Gigabyte) _formatUnit = Units.Gigabyte;
+					else if (value >= (int)Units.Megabyte) _formatUnit = Units.Megabyte;
+					else if (value >= (int)Units.Kilobyte) _formatUnit = Units.Kilobyte;
+					else _formatUnit = Units.Byte;
+				}
+				return _formatUnit.Value;
+			}
+			set { _formatUnit = value; }
+		}
+
+
 
 		/// <summary>
 		/// MaxFileSizeAttribute constructor
@@ -33,43 +58,42 @@ namespace Attributes.Validation
 		/// <param name="maxFileSize">Maximum file size in bytes</param>
 		public MaxFileSizeAttribute(long maxFileSize)
 		{
-			FormatUnit = Units.Byte;
 			_maxFileSize = maxFileSize;
 		}
+
+		/// <summary>
+		/// MaxFileSizeAttribute constructor
+		/// </summary>
+		/// <param name="maxFileSizeSettingKey">Key of the Web.config appSetting, where the max file size value must be taken from</param>
+		public MaxFileSizeAttribute(string maxFileSizeSettingKey)
+		{
+			var setting = ConfigurationManager.AppSettings[maxFileSizeSettingKey];
+			int value;
+			if (int.TryParse(setting, out value))
+				_maxFileSize = value;
+		}
+
+
 
 		public override bool IsValid(object value)
 		{
 			var file = value as HttpPostedFileBase;
-			if (file == null)
+			if (file == null || _maxFileSize <= 0)
 			{
-				return false;
+				return true;
 			}
 			return file.ContentLength <= _maxFileSize;
 		}
 
+
 		public override string FormatErrorMessage(string name)
 		{
-			const double _1024 = 1024d;
-			double value = _maxFileSize;
+			double value = Math.Round(_maxFileSize / (double)FormatUnit);
 
-			switch (FormatUnit)
-			{
-				case Units.Byte:
-					break;
-				case Units.Kilobyte:
-					value = value / _1024;
-					break;
-				case Units.Megabyte:
-					value = value / Math.Pow(_1024, 2);
-					break;
-				case Units.Gigabyte:
-					value = value / Math.Pow(_1024, 3);
-					break;
-			}
-
-			string size = string.Format("{0} {1}", Math.Round(value), _unitNames[FormatUnit]);
-			return string.Format(ErrorMessageString, name, size);
+			string sizeString = string.Format("{0} {1}", value, _unitNames[FormatUnit]);
+			return string.Format(ErrorMessageString, name, sizeString);
 		}
+
 
 		public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
 		{
